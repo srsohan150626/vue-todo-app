9b5efb0315c1d09b2420ba627f3558a39de4d35e
srsohan150626 <sohan.ice.pust@gmail.com>
2021-12-30 12:07:29 +0600

    finish

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -2,8 +2,8 @@
   <div class="container">
    <h2 class="text-center">My Vue Js Todo App</h2>
    <div class="d-flex">
-     <input type="text" placeholder="Enter task" class="form-control">
-     <button class="btn btn-warning rounded-0">SUBMIT</button>
+     <input v-model="task" type="text" placeholder="Enter task" class="form-control">
+     <button @click="submitask" class="btn btn-warning rounded-0">SUBMIT</button>
    </div>
    <div class="mt-4">
       <table class="table table-bordered">
@@ -17,10 +17,25 @@
   </thead>
   <tbody>
     <tr v-for="(task,index) in tasks" :key="index">
-      <td>{{task.name}}</td>
-      <td>{{task.status}}</td>
-      <td><div class="text-center"><span class="fa fa-pen"></span></div></td>
-      <td><div class="text-center"><span class="fa fa-trash"></span></div></td>
+      <td>
+         <span :class="{'finished': task.status=== 'finished'}">{{task.name}}</span>
+        </td>
+      <td>
+        <div :class="{'text-danger': task.status=== 'to-do', 'text-warning': task.status=== 'in-progrss','text-success': task.status=== 'finished'}" class="pointer" @click="changeStatus(index)"> 
+          {{task.status}}
+        </div>
+        
+        </td>
+      <td>
+        <div class="text-center"  @click="editTask(index)">
+          <span class="fa fa-pen"></span>
+        </div>
+      </td>
+      <td>
+        <div class="text-center" @click="deleteTask(index)">
+          <span class="fa fa-trash"></span>
+        </div>
+      </td>
     </tr>
   </tbody>
 </table>
@@ -34,6 +49,9 @@ export default {
   name: "TodoApp",
   data(){
     return {
+      task: "Hello task",
+      editedtask: null,
+      availableStatuses: ['to-do', 'in-progrss', 'finished'],
       tasks:[
         {
           name: "Read General Part",
@@ -49,6 +67,48 @@ export default {
         }
       ]
     }
+  },
+  methods:{
+    submitask(){
+      if(this.task.length === 0) {
+        alert("Please Write your task name");
+      }else{
+        if(this.editedtask === null){
+           this.tasks.push({
+          name: this.task,
+          status: "to-do"
+          })
+        }else{
+            this.tasks[this.editedtask].name = this.task;
+            this.editedtask = null;
+        }
+       
+
+      }
+      
+    },
+    deleteTask(index){
+      this.tasks.splice(index,1);
+    },
+
+    editTask(index){
+      this.task = this.tasks[index].name;
+      this.editedtask = index;
+    },
+    changeStatus(index){
+      let newIndex = this.availableStatuses.indexOf(this.tasks[index].status);
+      if(++newIndex>2) newIndex=0;
+      this.tasks[index].status = this.availableStatuses[newIndex];
+      //console.log(newIndex);
+    }
   }
 };
 </script>
+<style scoped>
+  .pointer{
+    cursor: pointer;
+  }
+  .finished{
+    text-decoration: line-through;
+  }
+</style>
