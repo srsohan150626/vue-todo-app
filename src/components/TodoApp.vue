e73e08c225ede0ec1acabd18d61dcab9c1f87ded
srsohan150626 <sohan.ice.pust@gmail.com>
2021-12-30 07:55:23 +0600

    update

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -1,11 +1,54 @@
 <template>
   <div class="container">
    <h2 class="text-center">My Vue Js Todo App</h2>
+   <div class="d-flex">
+     <input type="text" placeholder="Enter task" class="form-control">
+     <button class="btn btn-warning rounded-0">SUBMIT</button>
+   </div>
+   <div class="mt-4">
+      <table class="table table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">Tasks</th>
+      <th scope="col">Status</th>
+      <th scope="col" class="text-center">#</th>
+      <th scope="col" class="text-center">#</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(task,index) in tasks" :key="index">
+      <td>{{task.name}}</td>
+      <td>{{task.status}}</td>
+      <td><div class="text-center"><span class="fa fa-pen"></span></div></td>
+      <td><div class="text-center"><span class="fa fa-trash"></span></div></td>
+    </tr>
+  </tbody>
+</table>
+   </div>
+  
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoApp"
+  name: "TodoApp",
+  data(){
+    return {
+      tasks:[
+        {
+          name: "Read General Part",
+          status: "to-do"
+        },
+        {
+          name: "Read IT Part",
+          status: "to-do"
+        },
+        {
+          name: "Read Previus year questions",
+          status: "to-do"
+        }
+      ]
+    }
+  }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 createApp(App).use(store).use(router).mount("#app");
